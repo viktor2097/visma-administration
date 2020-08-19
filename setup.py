2e8e827ebebe0cbb3d9863e82c5f6214b9b1584f
Viktor Johansson <dpedesigns@hotmail.com>
2020-08-20 00:27:09 +0200

    Added support for readme on pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="visma_administration",
     packages=["visma_administration"],
-    version="0.1",
+    version="0.1.3",
     license="MIT",
     description="API for Visma Administration 200/500/1000/2000",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     author="Viktor Johansson",
     author_email="dpedesigns@hotmail.com",
     url="https://github.com/viktor2097/visma-administration",
-    download_url="https://github.com/viktor2097/visma-administration/archive/v_01.tar.gz",
+    download_url="https://github.com/viktor2097/visma-administration/archive/0.1.3.tar.gz",
     keywords=["visma"],
     install_requires=["pythonnet>=2.5.1"],
     classifiers=[
