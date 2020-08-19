667f1e60ef08f91633c90ca104d78df32faf394c
Viktor Johansson <dpedesigns@hotmail.com>
2020-08-20 00:29:08 +0200

    setup missing semicolon

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -13,7 +14,7 @@ setup(
     license="MIT",
     description="API for Visma Administration 200/500/1000/2000",
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
     author="Viktor Johansson",
     author_email="dpedesigns@hotmail.com",
     url="https://github.com/viktor2097/visma-administration",
